431c82b5f21567bc33ac4852de4c6e264306292f
rishabhmwb <rishabhsingh@makewebbetter.com>
2022-12-23 08:51:45 +0530

    timing issue fixed

--- public/js/mwb-public.js
+++ public/js/mwb-public.js
@@ -5,11 +5,13 @@ jQuery(document).ready(function($){
         jQuery(this).siblings('.mwb-mbfw-user-booking-meta-data-listing').slideToggle('slow');
     })
 
-
+if( mwb_mbfw_public_obj.daily_start_time != '' && mwb_mbfw_public_obj.daily_end_time != '' ) {
+    
     $('.mwb_mbfw_time_date_picker_frontend').datetimepicker({
         format  : 'd-m-Y H:i',
         minTime: mwb_mbfw_public_obj.daily_start_time,
         maxTime : mwb_mbfw_public_obj.daily_end_time + 1,
         // minTime : mwb_mbfw_common_obj.minTime
     });
+}
 });

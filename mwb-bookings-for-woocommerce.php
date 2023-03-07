4aa3b4e493fa277fe0ee995985fbdcbee2faaada
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-03-07 15:01:16 +0530

    language file added

--- mwb-bookings-for-woocommerce.php
+++ mwb-bookings-for-woocommerce.php
@@ -21,9 +21,9 @@
  * Text Domain:       mwb-bookings-for-woocommerce
  * Domain Path:       /languages
  *
- * Requires at least:    5.1.0
+ * Requires at least:    5.5.0
  * Tested up to:         6.1.1
- * WC requires at least: 5.1.0
+ * WC requires at least: 5.5.0
  * WC tested up to:      7.4.1
  * Requires PHP:         7.2
  * Stable tag:           3.0.7

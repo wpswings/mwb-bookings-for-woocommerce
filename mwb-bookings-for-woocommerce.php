d31c77066f9c697cd719366f4b121ff1ea7d1112
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-08-16 16:45:36 +0530

    v3.1.2

--- mwb-bookings-for-woocommerce.php
+++ mwb-bookings-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least:    5.5.0
- * Tested up to:         6.2.2
+ * Tested up to:         6.3.0
  * WC requires at least: 5.5.0
- * WC tested up to:      7.9.0
+ * WC tested up to:      8.0.2
  * Requires PHP:         7.2
  * Stable tag:           3.1.2
  *

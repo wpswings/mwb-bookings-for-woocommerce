0ff1a80321f53d8ef33dc391aac62f1026debd46
Stuti-Cedcoss <stutinegi@makewebbetter.com>
2025-05-30 17:24:08 +0530

    issue test push

--- common/class-mwb-bookings-for-woocommerce-common.php
+++ common/class-mwb-bookings-for-woocommerce-common.php
@@ -497,14 +497,14 @@ class Mwb_Bookings_For_Woocommerce_Common {
 				$wps_general_price = apply_filters( 'wps_mbfw_set_unit_cost_price_hour', $product_price, $product_id, $date_time_from, $date_time_to, $unit );
 			}
 		} else {
-			$active_plugins = get_option( 'active_plugins' );
-			if ( ! in_array( 'bookings-for-woocommerce-pro/bookings-for-woocommerce-pro.php', $active_plugins ) ) {
+			// $active_plugins = get_option( 'active_plugins' );
+			// if ( ! in_array( 'bookings-for-woocommerce-pro/bookings-for-woocommerce-pro.php', $active_plugins ) ) {
 				$wps_unv_day = wps_booking_get_meta_data( $product_id, 'mwb_mbfw_choose_holiday', true );
 				if ( strtotime( $date_time_from ) < strtotime( $wps_unv_day ) && strtotime( $date_time_to ) > strtotime( $wps_unv_day ) ) {
 					echo 'fail';
 					wp_die();
 				}
-			}
+			// }
 
 			if ( 'day' === wps_booking_get_meta_data( $product_id, 'mwb_mbfw_booking_unit', true ) && ! empty( $date_time_to ) && ! empty( $date_time_from ) ) {
 				$from_timestamp    = strtotime( $date_from );
@@ -524,7 +524,7 @@ class Mwb_Bookings_For_Woocommerce_Common {
 		$wps_bfwp_msg = apply_filters( 'wps_mbfw_check_availablity', $product_id, $date_time_from, $date_time_to );
 
 		if ( 'fail' === $wps_bfwp_msg ) {
-			return 'fail';
+			echo 'fail';
 			wp_die();
 		}
 		$services_cost = $this->mbfw_extra_service_charge( $product_id, $services_checked, $service_quantity, $people_number, $unit );

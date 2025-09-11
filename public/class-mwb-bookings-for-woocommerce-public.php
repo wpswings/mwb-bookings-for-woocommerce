6ec569512c7afe708166d88bbf89a63fb6d02412
Stuti-Cedcoss <stutinegi@makewebbetter.com>
2025-09-11 18:38:00 +0530

    critical issue fix

--- public/class-mwb-bookings-for-woocommerce-public.php
+++ public/class-mwb-bookings-for-woocommerce-public.php
@@ -480,7 +480,6 @@ class Mwb_Bookings_For_Woocommerce_Public {
 
 				wp_enqueue_script( 'mwb-mbfw-select2', MWB_BOOKINGS_FOR_WOOCOMMERCE_DIR_URL . 'package/lib/select-2/mwb-bookings-for-woocommerce-select2.js', array( 'jquery' ), time(), false );
 				// Your plugin JS.
-				mwb-public-form.css
 				wp_enqueue_script('booking-calendar-js', plugin_dir_url(__FILE__) . 'js/mwb-booking-public-form.js', ['jquery'], null, true);
 				wp_enqueue_script('booking-calendar-js', plugin_dir_url(__FILE__) . 'js/mwb-global-booking-shortcode.js', ['fullcalendar-js','mwb-mbfw-select2'], null, true);
 				$container_id = 'booking-calendar-' . esc_attr($post_id);

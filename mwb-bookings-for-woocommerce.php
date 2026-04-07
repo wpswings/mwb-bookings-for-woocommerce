155b69ccb3205597a332aff20bf5ad168c6e7db1
Stuti-Cedcoss <stutinegi@makewebbetter.com>
2026-04-07 11:22:18 +0530

    fix : plugin version change

--- mwb-bookings-for-woocommerce.php
+++ mwb-bookings-for-woocommerce.php
@@ -23,11 +23,11 @@
  *
  * Requires Plugins: woocommerce
  * Requires at least:    6.7.0
- * Tested up to:         6.9.1
+ * Tested up to:         6.9
  * WC requires at least: 6.5.0
- * WC tested up to:      10.5.2
+ * WC tested up to:      10.6.2
  * Requires PHP:         7.4
- * Stable tag:           3.11.2
+ * Stable tag:           3.11.3
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html

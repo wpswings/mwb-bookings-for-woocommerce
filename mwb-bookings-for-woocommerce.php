dd69152ae471c919e9ac379a2f582cb766497137
Stuti-Cedcoss <stutinegi@makewebbetter.com>
2026-04-10 12:11:23 +0530

    new relesse branch

--- mwb-bookings-for-woocommerce.php
+++ mwb-bookings-for-woocommerce.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Bookings for WooCommerce
  * Plugin URI:        https://wordpress.org/plugins/mwb-bookings-for-woocommerce/
  * Description:        <code><strong>Bookings for WooCommerce</strong></code> enable store owners to create an online booking system that allows them to turn their products into Booking Solutions.<a href="https://wpswings.com/woocommerce-plugins/?utm_source=wpswings-bookings&utm_medium=bookings-org-backend&utm_campaign=official" target="_blank"> Elevate your e-commerce store by exploring more on <strong> WP Swings </strong></a>.
- * Version:           3.11.3
+ * Version:           3.11.4
  * Author:            WP Swings
  * Author URI:        https://wpswings.com/?utm_source=wpswings-bookings-official&utm_medium=bookings-org-page&utm_campaign=official
  * Text Domain:       mwb-bookings-for-woocommerce
@@ -27,7 +27,7 @@
  * WC requires at least: 6.5.0
  * WC tested up to:      10.6.2
  * Requires PHP:         7.4
- * Stable tag:           3.11.3
+ * Stable tag:           3.11.4
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html

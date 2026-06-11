2da2e392fab59d64c69c1b34adf3c41c8fe6d10d
Stuti-Cedcoss <stutinegi@makewebbetter.com>
2026-06-11 16:46:14 +0530

    latest WP/WC updated

--- includes/class-mwb-bookings-for-woocommerce.php
+++ includes/class-mwb-bookings-for-woocommerce.php
@@ -86,7 +86,7 @@ class Mwb_Bookings_For_Woocommerce {
 
 			$this->version = MWB_BOOKINGS_FOR_WOOCOMMERCE_VERSION;
 		} else {
-			$this->version = '3.11.6';
+			$this->version = '3.11.7';
 		}
 
 		$this->plugin_name = 'bookings-for-woocommerce';
